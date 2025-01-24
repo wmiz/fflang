1183d0b71fb3a9cd94279ebaa5adee134aab80db
Will Misback <wmiz@Wills-Mac-mini.local>
2025-01-24 10:39:58 -0600

    Fix header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,4 @@
 <script>
-  import NavLink from "./NavLink.svelte";
   import logo from "$lib/img/LOGO.png";
 
     const links = [
@@ -10,21 +9,55 @@
     ]
 </script>
 
-<header class="fixed w-full bg-white shadow-md z-50">
-    <nav class="container mx-auto px-4 py-4 flex items-center justify-between">
-        <div class="flex items-center">
-            <img src={logo} alt="Fast &amp; Fun Language Logo" class="h-12">
+<div class="navbar bg-base-100 shadow-sm">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16" />
+          </svg>
         </div>
-        <div class="mx-3 hidden md:flex space-x-6">
-            {#each links as link}
-                <NavLink href={link.href} title={link.name} />
-            {/each}
-        </div>
-        <div class="flex items-center space-x-4">
-            <a href="/enroll" class="btn-text bg-brand-primary text-white px-4 py-2 rounded-lg hover:opacity-90">Enroll Now</a>
-            <a href="#login" class="student-icon">
-                <i class="fas fa-user text-gray-400"></i>
-            </a>
-        </div>
-    </nav>
-</header>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+          <li><a>Item 1</a></li>
+          <li>
+            <a>Parent</a>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </li>
+          <li><a>Item 3</a></li>
+        </ul>
+      </div>
+      <img src={logo} alt="logo" class="h-10" />
+    </div>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        <li><a>Item 1</a></li>
+        <li>
+          <details>
+            <summary>Parent</summary>
+            <ul class="p-2">
+              <li><a>Submenu 1</a></li>
+              <li><a>Submenu 2</a></li>
+            </ul>
+          </details>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <a class="btn">Buy Now</a>
+    </div>
+  </div>
