6b464cfe6e06ed9a18b6d2ea1388b486df41f5ee
Will Misback <wmiz@Wills-Mac-mini.local>
2025-03-27 15:02:45 -0600

    fix

--- src/components/HomePage/Card.svelte
+++ src/components/HomePage/Card.svelte
@@ -8,7 +8,7 @@
   
   <div class="card text-center bg-white md:w-1/3 w-full shadow-2xl rounded-md bg-white">
     <div class="card-body flex-none p-8 text-left">
-      <h2 class="card-title justify-center p-2 rounded-lg text-4xl font-accent mb-2 uppercase bg-[#EE6352] shadow-md text-white w-full">{title}</h2>
+      <h2 class="card-title justify-center p-2 rounded-lg md:text-4xl text-2xl font-accent mb-2 uppercase bg-[#EE6352] shadow-md text-white w-full">{title}</h2>
       <img src={imageSrc} width="80" class="m-auto mb-2" alt="" />
       <h2 class="card-title m-auto text-3xl font-accent italic">{subtitle}</h2>
       <p class="text-xl font-bold text-center">{duration}</p>
