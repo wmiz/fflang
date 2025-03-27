9a4d9abf315953f6ae7a6c36e2fc67dc573908ec
Will Misback <wmiz@Wills-Mac-mini.local>
2025-03-27 15:10:48 -0600

    Fix

--- src/components/HomePage/CTA.svelte
+++ src/components/HomePage/CTA.svelte
@@ -27,7 +27,7 @@
   <h1 class="md:text-6xl text-3xl font-accent font-bold mb-8 text-center ">
     My Goal with this Program…
   </h1>
-  <p class="text-2xl mb-8">
+  <p class="md:text-2xl text-lg mb-8">
     My Goal with this program is to give 10X the Value of the Price…Let’s see if
     I achieve it…Here’s what you get…
   </p>
@@ -51,28 +51,28 @@
     </ul>
   </section>
 
-  <section class="text-4xl text-center font-accent mt-16">
-    <p class=" text-[#EE6352] font-bold">
+  <section class="md:text-4xl text-lg text-center font-accent md:mt-16 mt-12">
+    <p class=" text-[#EE6352] font-bold md:text-4xl text-2xl">
       Total Value = <span class="text-[#EE6352]">${totalValue.toFixed(2)}</span>
     </p>
     <p class="mb-4">(Not including the “Priceless” Inner Transformation)</p>
-    <p class="font-bold">
+    <p class="font-bold text-2xl md:text-4xl">
       Originally, $297      
 
-      <span class="text-4xl ml-2 font-bold text-blue-500">Now, Only $197</span>
+      <span class="text-2xl md:text-4xl ml-2 font-bold text-blue-500">Now, Only $197</span>
     </p>
     <div class="text-center font-body py-8">
       <a href="/book">
         <button
-          class="btn bg-[#EE6352] rounded-full bg-[#000000] text-white text-3xl p-8 uppercase shadow-lg hover:underline hover:scale-102 transition-transform duration-50 ease-in-out"
+          class="btn bg-[#EE6352] rounded-full bg-[#000000] text-white text-3xl md:p-8 p-6 uppercase shadow-lg hover:underline hover:scale-102 transition-transform duration-50 ease-in-out"
           >Book Now</button
         >
       </a>
     </div>
-    <h3 class="text-center font-bold uppercase text-[#1877F2] text-5xl mb-8">Did I Reach my Goal?</h3>
-    <p class="text-left mb-4 text-2xl">There it is…all laid out out. Did I reach my goal to give 10X the value? It all comes down to how much you value the Inner Transformation. Only you can decide.
+    <h3 class="text-center font-bold uppercase text-[#1877F2] text-3xl md:text-5xl mb-8">Did I Reach my Goal?</h3>
+    <p class="text-left mb-4 md:text-2xl">There it is…all laid out out. Did I reach my goal to give 10X the value? It all comes down to how much you value the Inner Transformation. Only you can decide.
     </p>
-    <p class='text-left mb-10 text-2xl'>Hope to meet you and your child…and embark upon the next adventure together…with your child as one of the 8!
+    <p class='text-left mb-5 md:mb-10 md:text-2xl'>Hope to meet you and your child…and embark upon the next adventure together…with your child as one of the 8!
     </p>
   </section>
 </div>
